78e6d71893a4540ba62dbfdc01e9f6a9cd94b1e0
Leonardo Rodrigues <leovictorsr@gmail.com>
2022-11-21 13:50:17 -0300

    fix averiecooks

--- scrapers/averiecooks.js
+++ scrapers/averiecooks.js
@@ -1,85 +1,54 @@
-const request = require("request");
 const cheerio = require("cheerio");
-
+const puppeteerFetch = require("../helpers/puppeteerFetch");
 const RecipeSchema = require("../helpers/recipe-schema");
 
 const averieCooks = url => {
-  const Recipe = new RecipeSchema();
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     if (!url.includes("averiecooks.com")) {
       reject(new Error("url provided must include 'averiecooks.com'"));
     } else {
-      request(url, (error, response, html) => {
-        if (!error && response.statusCode === 200) {
-          const $ = cheerio.load(html);
+      try {
+        const html = await puppeteerFetch(url);
+        const Recipe = new RecipeSchema();
+        const $ = cheerio.load(html);
 
-          Recipe.image = $("meta[property='og:image']").attr("content");
-          Recipe.name = $(".innerrecipe")
-            .children("h2")
-            .first()
-            .text();
+        Recipe.name = $("meta[property='og:title']").attr("content");
+        Recipe.image = $("meta[property='og:image']").attr("content");
 
-          $(".cookbook-ingredients-list")
-            .children("li")
-            .each((i, el) => {
-              Recipe.ingredients.push(
-                $(el)
-                  .text()
-                  .trim()
-                  .replace(/\s\s+/g, " ")
-              );
-            });
-
-          $(".instructions")
-            .find("li")
-            .each((i, el) => {
-              Recipe.instructions.push($(el).text());
-            });
-
-          $(".recipe-meta")
-            .children("p")
-            .each((i, el) => {
-              const title = $(el)
-                .children("strong")
+        $("div.mv-create-ingredients")
+          .children("li")
+          .each((i, el) => {
+            Recipe.ingredients.push(
+              $(el)
                 .text()
-                .replace(/\s*:|\s+(?=\s*)/g, "");
-              const value = $(el)
-                .text()
-                .replace(/[^:]*:/, "")
-                .trim();
-              switch (title) {
-                case "PrepTime":
-                  Recipe.time.prep = value;
-                  break;
-                case "CookTime":
-                  Recipe.time.cook = value;
-                  break;
-                case "TotalTime":
-                  Recipe.time.total = value;
-                  break;
-                case "Yield":
-                  Recipe.servings = value;
-                  break;
-                default:
-                  break;
-              }
-            });
+                .trim()
+                .replace(/\s\s+/g, " ")
+            );
+          });
 
-          if (
-            !Recipe.name ||
-            !Recipe.ingredients.length ||
-            !Recipe.instructions.length
-          ) {
-            reject(new Error("No recipe found on page"));
-          } else {
-            resolve(Recipe);
-          }
-        } else {
+        $("div.mv-create-instructions")
+          .find("li")
+          .each((i, el) => {
+            Recipe.instructions.push($(el).text());
+          });
+
+
+        Recipe.time.prep = $(".mv-create-time-prep").text();
+        Recipe.time.cook = $(".mv-create-time-active").text();
+        Recipe.time.total = $(".mv-create-time-total").text();
+
+        if (!Recipe.name || !Recipe.ingredients.length) {
           reject(new Error("No recipe found on page"));
+        } else {
+          resolve(Recipe);
         }
-      });
+      } catch (error) {
+        reject(new Error("No recipe found on page"));
+      }
     }
   });
-};
+};;
+
+averieCooks("https://www.averiecooks.com/chicken-pad-thai").then(Recipe => console.log(Recipe));
 
 module.exports = averieCooks;
