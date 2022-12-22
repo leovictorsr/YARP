0c4b1bfdc0769d67b0af7c44e607dae7776244f9
Leonardo Rodrigues <leovictorsr@gmail.com>
2022-12-22 18:18:02 -0300

    envelop puppeteerFetch with a try catch for puppeteer crashes

--- helpers/puppeteerFetch.js
+++ helpers/puppeteerFetch.js
@@ -38,95 +38,99 @@ const skippedResources = [
 ];
 
 const puppeteerFetch = async url => {
-  const args = [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-infobars',
-    '--window-position=0,0',
-    '--ignore-certifcate-errors',
-    '--ignore-certifcate-errors-spki-list',
-    "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
-  ];
-  const options = {
-    args,
-    headless: true,
-    ignoreHTTPSErrors: true,
-    userDataDir: './tmp'
-  };
+  try {
+    const args = [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-infobars',
+      '--window-position=0,0',
+      '--ignore-certifcate-errors',
+      '--ignore-certifcate-errors-spki-list',
+      "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    ];
+    const options = {
+      args,
+      headless: true,
+      ignoreHTTPSErrors: true,
+      userDataDir: './tmp'
+    };
 
-  const browser = await puppeteer.launch(options);
+    const browser = await puppeteer.launch(options);
 
-  const page = (await browser.pages())[0];
+    const page = (await browser.pages())[0];
 
-  await page.evaluateOnNewDocument(() => {
-    Object.defineProperty(navigator, 'webdriver', {
-      get: () => false,
+    await page.evaluateOnNewDocument(() => {
+      Object.defineProperty(navigator, 'webdriver', {
+        get: () => false,
+      });
     });
-  });
 
-  await page.setRequestInterception(true);
-  await page.on("request", req => {
-    const requestUrl = req._url.split("?")[0].split("#")[0];
-    if (
-      blockedResourceTypes.indexOf(req.resourceType()) !== -1 ||
-      skippedResources.some(resource => requestUrl.indexOf(resource) !== -1)
-    ) {
-      req.abort();
+    await page.setRequestInterception(true);
+    await page.on("request", req => {
+      const requestUrl = req._url.split("?")[0].split("#")[0];
+      if (
+        blockedResourceTypes.indexOf(req.resourceType()) !== -1 ||
+        skippedResources.some(resource => requestUrl.indexOf(resource) !== -1)
+      ) {
+        req.abort();
+      } else {
+        req.continue();
+      }
+    })
+
+    // Pass the Permissions Test.
+    await page.evaluateOnNewDocument(() => {
+      const originalQuery = window.navigator.permissions.query;
+      return window.navigator.permissions.query = (parameters) => (
+        parameters.name === 'notifications' ?
+          Promise.resolve({ state: Notification.permission }) :
+          originalQuery(parameters)
+      );
+    });
+
+    // Pass the Plugins Length Test.
+    await page.evaluateOnNewDocument(() => {
+      // Overwrite the `plugins` property to use a custom getter.
+      Object.defineProperty(navigator, 'plugins', {
+        // This just needs to have `length > 0` for the current test,
+        // but we could mock the plugins too if necessary.
+        get: () => [1, 2, 3, 4, 5],
+      });
+    });
+
+    // Pass the Languages Test.
+    await page.evaluateOnNewDocument(() => {
+      // Overwrite the `plugins` property to use a custom getter.
+      Object.defineProperty(navigator, 'languages', {
+        get: () => ['en-US', 'en'],
+      });
+    });
+
+    const domain = "https://" + url.split("/")[2];
+    await page.setCookie({ name: 'Secure', value: 'true', domain });
+
+    const response = await page.goto(url);
+
+    if (response && response._status < 400) {
+      let html = await page.content();
+      try {
+        await browser.close();
+      } finally {
+        return html;
+      }
     } else {
-      req.continue();
+      try {
+        await browser.close();
+      } finally {
+        let rejection;
+
+        if (!response) rejection = "No response from the site";
+        else if (response._status) rejection = "Response received: " + response._status;
+        return Promise.reject(rejection);
+      }
     }
-  })
-
-  // Pass the Permissions Test.
-  await page.evaluateOnNewDocument(() => {
-    const originalQuery = window.navigator.permissions.query;
-    return window.navigator.permissions.query = (parameters) => (
-      parameters.name === 'notifications' ?
-        Promise.resolve({ state: Notification.permission }) :
-        originalQuery(parameters)
-    );
-  });
-
-  // Pass the Plugins Length Test.
-  await page.evaluateOnNewDocument(() => {
-    // Overwrite the `plugins` property to use a custom getter.
-    Object.defineProperty(navigator, 'plugins', {
-      // This just needs to have `length > 0` for the current test,
-      // but we could mock the plugins too if necessary.
-      get: () => [1, 2, 3, 4, 5],
-    });
-  });
-
-  // Pass the Languages Test.
-  await page.evaluateOnNewDocument(() => {
-    // Overwrite the `plugins` property to use a custom getter.
-    Object.defineProperty(navigator, 'languages', {
-      get: () => ['en-US', 'en'],
-    });
-  });
-
-  const domain = "https://" + url.split("/")[2];
-  await page.setCookie({ name: 'Secure', value: 'true', domain });
-  
-  const response = await page.goto(url);
-
-  if (response && response._status < 400) {
-    let html = await page.content();
-    try {
-      await browser.close();
-    } finally {
-      return html;
-    }
-  } else {
-    try {
-      await browser.close();
-    } finally {
-      let rejection;
-
-      if (!response) rejection = "No response from the site";
-      else if (response._status) rejection = "Response received: " + response._status;
-      return Promise.reject(rejection);
-    }
+  } catch (e) {
+    return Promise.reject(e);
   }
 };
 
